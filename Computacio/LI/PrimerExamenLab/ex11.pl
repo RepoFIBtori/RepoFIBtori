1d69d793ac91caa5fecc58b76933570f8f5dd12a
adria <adriacabeza@hotmail.com>
2019-06-02 12:57:24 +0200

    borrant la merda de prolog

--- Computacio/LI/PrimerExamenLab/ex11.pl
+++
@@ -1,5 +0,0 @@
-ordenada([_|[]]).
-ordenada([X1,X2|L]) :- X1 =< X2, ordenada([X2|L]).
-
-
-esta_ordenada(L) :- ordenada(L), !, fail.
--- Computacio/LI/PrimerExamenLab/ex2.pl
+++
@@ -1,3 +0,0 @@
-prod([],1).
-prod([X|L],P) :- prod(L,P1), P is P1*X.
-
